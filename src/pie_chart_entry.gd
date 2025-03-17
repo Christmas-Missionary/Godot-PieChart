b905c25e61db9571aac27793905da3205115da12
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2025-03-17 15:55:00 +0000

    export range for PieChartEntry

--- src/pie_chart_entry.gd
+++ src/pie_chart_entry.gd
@@ -2,7 +2,7 @@ extends Resource
 class_name PieChartEntry
 
 @export var name: String
-@export var weight: float
+@export_range(0, 2 ** 64) var weight: float
 @export var color: Color
 
 func _init(name_: String = "", weight_: float = 0.0, color_: Color = Color.BLACK) -> void:
