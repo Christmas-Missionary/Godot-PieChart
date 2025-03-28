3f8ca132759a3b2e48f18d10314467c0dd7ede23
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2025-03-28 13:27:39 -0700

    inlined circle drawer, assign script to Node2D

--- src/circle_drawer.gd
+++ src/circle_drawer.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func _draw() -> void:
 	var parent: PieChartTitleLabel = get_parent() as PieChartTitleLabel
-	assert(parent != null, "Parent is not a PieChartTitleLabel!")
+	assert(parent != null, "Parent is not a PieChartTitleLabel! You are not supposed to assign this script to a child of a non-PieChartTitleLabel!")
 	draw_circle(parent.size / 2, parent.circle_radius, parent.circle_color)
--- src/title_label.gd
+++ src/title_label.gd
@@ -1,5 +1,7 @@
 class_name PieChartTitleLabel extends RichTextLabel
 
+var _drawer: Node2D = Node2D.new()
+
 @export_group("Circle", "circle_")
 @export var circle_color: Color:
 	set(val):
@@ -8,12 +10,12 @@ class_name PieChartTitleLabel extends RichTextLabel
 
 @export_range(0, 1000) var circle_radius: float:
 	set(val):
-		assert(circle_radius >= 0, "Someone changed the range of `title_circle_radius` in PieChart!")
 		circle_radius = val
 		_drawer.queue_redraw()
 
-var _drawer: Node2D = preload("res://src/circle_drawer.tscn").instantiate() as Node2D
-
 func _ready() -> void:
+	assert(get_parent() as Control != null, "Parent is not a Control node!")
 	size = (get_parent() as Control).size
 	add_child(_drawer, false, Node.INTERNAL_MODE_FRONT)
+	_drawer.show_behind_parent = true
+	_drawer.set_script(preload("res://src/circle_drawer.gd"))
