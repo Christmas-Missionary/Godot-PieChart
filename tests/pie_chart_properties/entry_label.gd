9555f12df0f8fc38e46432f469e7c7ff2a806e54
Christmas-Missionary <156368675+Christmas-Missionary@users.noreply.github.com>
2025-04-02 15:29:57 +0000

    improved tests

--- tests/pie_chart_properties/entry_label.gd
+++ tests/pie_chart_properties/entry_label.gd
@@ -12,24 +12,20 @@ func _ready() -> void:
 	super()
 	set_physics_process(false)
 
-func _unhandled_key_input(_event: InputEvent) -> void:
-	if Input.is_physical_key_pressed(KEY_SPACE):
-		for node: Node in _rects:
-			node.queue_free()
-		_rects = []
-		rads_rotated = 0.0
-		set_physics_process(true)
-		last_rot = _chart.starting_offset_radians
+func _on_trace_button_pressed() -> void:
+	_rects = []
+	rads_rotated = 0.0
+	set_physics_process(true)
+	last_rot = _chart.starting_offset_radians
 
 func _physics_process(_delta: float) -> void:
-	var to_drop: ColorRect = ColorRect.new()
+	var to_drop: ColorRect = preload("res://tests/pie_chart_properties/rect.tscn").instantiate() as ColorRect
 	_rects.push_back(to_drop)
 	_main.add_child(to_drop)
-	to_drop.size = Vector2.ONE * 10
 	to_drop.global_position = get_global_rect().position + get_global_rect().size / 2
-	
 	if rads_rotated >= TAU:
 		set_physics_process(false)
+		(_main.get_node("TraceButton") as CanvasItem).show()
 	else:
 		rads_rotated += (_chart.starting_offset_radians - last_rot)
 		last_rot = _chart.starting_offset_radians
